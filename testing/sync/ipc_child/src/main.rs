a029fe300dd103349631fabe1644d59e767432b1
David Alsh <alshdavid@gmail.com>
2024-05-11 14:37:11 +1000

    benchmark

---
+++ testing/sync/ipc_child/src/main.rs
@@ -0,0 +1,43 @@
+use std::env;
+use std::thread;
+
+use clap::Parser;
+use ipc_adapter::child::sync::HostReceiver;
+use ipc_adapter::child::sync::HostSender;
+
+#[derive(Parser, Debug, Clone)]
+struct Config {
+  #[arg(short = 'b', long = "benchmark", env = "IPC_BENCHMARK")]
+  pub benchmark: bool,
+}
+
+fn main() {
+  let config = Config::parse();
+
+  let host_receiver = HostReceiver::<usize, usize>::new(&env::var("IPC_CHANNEL_HOST_OUT").unwrap());
+  let host_sender =
+    HostSender::<usize, usize>::new(&env::var("IPC_CHANNEL_HOST_IN").unwrap()).unwrap();
+
+
+  // If not running benchmark
+  if !config.benchmark {
+    let rx = host_receiver.on.subscribe();
+
+    thread::spawn(move || {
+      while let Ok((v, reply)) = rx.recv() {
+        println!("[Child] Received: {}", v);
+        reply.send(v).unwrap()
+      }
+    });
+
+    let response = host_sender.send_blocking(43);
+    println!("[Child] Response: {}", response);
+    return;
+  }
+
+  // Benchmark responder
+  let rx = host_receiver.on.subscribe();
+  while let Ok((v, reply)) = rx.recv() {
+    reply.send(v).unwrap()
+  }
+}
---
+++ testing/sync/ipc_host/src/main.rs
@@ -0,0 +1,72 @@
+use std::process::Command;
+use std::process::Stdio;
+use std::thread;
+use std::time::SystemTime;
+
+use clap::Parser;
+use ipc_adapter::host::sync::ChildReceiver;
+use ipc_adapter::host::sync::ChildSender;
+
+#[derive(Parser, Debug, Clone)]
+struct Config {
+  #[arg(short = 'b', long = "benchmark", env = "IPC_BENCHMARK")]
+  pub benchmark: bool,
+
+  #[arg(short = 'm',long = "benchmark-message-count", env = "IPC_BENCHMARK_MESSAGE_COUNT", default_value = "100000")]
+  pub benchmark_message_count: usize,
+}
+
+fn main() {
+  let config = Config::parse();
+
+  let child_sender = ChildSender::<usize, usize>::new();
+  let child_receiver = ChildReceiver::<usize, usize>::new();
+
+  let entry = std::env::current_exe()
+    .unwrap()
+    .parent()
+    .unwrap()
+    .join("ipc_child_sync")
+    .to_owned();
+
+  let mut command = Command::new(entry.to_str().unwrap());
+  command.env("IPC_CHANNEL_HOST_OUT", &child_sender.server_name);
+  command.env("IPC_CHANNEL_HOST_IN", &child_receiver.server_name);
+  command.env("IPC_BENCHMARK", &config.benchmark.to_string());
+
+  command.stderr(Stdio::inherit());
+  command.stdout(Stdio::inherit());
+  command.stdin(Stdio::piped());
+
+  command.spawn().unwrap();
+
+  // If not running benchmark
+  if !config.benchmark {
+    let rx = child_receiver.on.subscribe();
+
+    thread::spawn(move || {
+      while let Ok((v, reply)) = rx.recv() {
+        println!("[Host] Received: {}", v);
+        reply.send(v).unwrap()
+      }
+    });
+
+    let response = child_sender.send_blocking(42);
+    println!("[Host] Response: {}", response);
+    return
+  }
+  
+  println!("Benchmark: host sending \"{}\" messages", config.benchmark_message_count);
+
+  // Benchmark mode
+  let start_time = SystemTime::now();
+  for _ in 0..config.benchmark_message_count {
+    child_sender.send_blocking(42);
+  }
+  let end_time = start_time.elapsed().unwrap();
+
+  println!(
+    "Total Time (ms): {:.3}s",
+    end_time.as_nanos() as f64 / 1_000_000 as f64 / 1000 as f64
+  );
+}
